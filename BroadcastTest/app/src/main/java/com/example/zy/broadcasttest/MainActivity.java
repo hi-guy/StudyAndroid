4094bb4f5f5ae2bd98bd9765e45954246c209a90
DESKTOP-UF412J8\zy <zhouyi.guy@gmail.com>
2017-02-12 12:26:01 +0800

    broadcast done

--- BroadcastTest/app/src/main/java/com/example/zy/broadcasttest/MainActivity.java
+++ BroadcastTest/app/src/main/java/com/example/zy/broadcasttest/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,40 +14,64 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+//    private IntentFilter intentFilter;
+//    private NetworkChangeReceiver networkChangeReceiver;
+
     private IntentFilter intentFilter;
-    private NetworkChangeReceiver networkChangeReceiver;
+    private LocalReceiver localReceiver;
+    private LocalBroadcastManager localBroadcastManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        intentFilter = new IntentFilter();
+//        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+//        networkChangeReceiver = new NetworkChangeReceiver();
+//        registerReceiver(networkChangeReceiver, intentFilter);
+
+        localBroadcastManager = LocalBroadcastManager.getInstance(this);
         intentFilter = new IntentFilter();
-        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
-        networkChangeReceiver = new NetworkChangeReceiver();
-        registerReceiver(networkChangeReceiver, intentFilter);
+        intentFilter.addAction("com.example.broadcasttest.LOCAL_BROADCAST");
+        localReceiver = new LocalReceiver();
+        localBroadcastManager.registerReceiver(localReceiver, intentFilter);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        unregisterReceiver(networkChangeReceiver);
+//        unregisterReceiver(networkChangeReceiver);
+        localBroadcastManager.unregisterReceiver(localReceiver);
     }
 
-    private class NetworkChangeReceiver extends BroadcastReceiver {
+    private class LocalReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
-            ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
-            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-            if (networkInfo != null && networkInfo.isAvailable()) {
-                Toast.makeText(context, "network is available", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(context, "network is unavailable", Toast.LENGTH_SHORT).show();
-            }
+            Toast.makeText(context, "received local broadcast", Toast.LENGTH_SHORT).show();
         }
     }
 
+//    private class NetworkChangeReceiver extends BroadcastReceiver {
+//        @Override
+//        public void onReceive(Context context, Intent intent) {
+//            ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+//            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+//            if (networkInfo != null && networkInfo.isAvailable()) {
+//                Toast.makeText(context, "network is available", Toast.LENGTH_SHORT).show();
+//            } else {
+//                Toast.makeText(context, "network is unavailable", Toast.LENGTH_SHORT).show();
+//            }
+//        }
+//    }
+
     public void sendBroadcast(View view) {
-        Intent intent = new Intent("com.example.zy.broadcasttest.MY_BROADCAST");
-        sendBroadcast(intent);
+//        Intent intent = new Intent("com.example.zy.broadcasttest.MY_BROADCAST");
+////        sendBroadcast(intent);
+//        sendOrderedBroadcast(intent, null);
+
+        Intent intent = new Intent("com.example.broadcasttest.LOCAL_BROADCAST");
+        localBroadcastManager.sendBroadcast(intent);
     }
+
 }
